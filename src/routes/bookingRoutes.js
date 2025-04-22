2b1c095e4158f6434bb3386c5494622c9f5bd9e1
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-04-22 21:20:12 +0300

    feat(booking): implement routes for booking management (create, read, update, delete)

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -1,23 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bookingController = require('../controllers/bookingController');
+const bookingController = require("../controllers/bookingController");
 
 // Create a new booking
-router.post('/', bookingController.createBooking);
+router.post("/", bookingController.createBooking);
 
 // Get all bookings
-router.get('/', bookingController.getAllBookings);
+router.get("/", bookingController.getAllBookings);
 
 // Get booking by ID
-router.get('/:id', bookingController.getBookingById);
+router.get("/:id", bookingController.getBookingById);
 
 // Update booking status
-router.patch('/:id/status', bookingController.updateBookingStatus);
+router.patch("/:id/status", bookingController.updateBookingStatus);
 
 // Delete booking
-router.delete('/:id', bookingController.deleteBooking);
+router.delete("/:id", bookingController.deleteBooking);
 
 // Get bookings by date range
-router.get('/date-range', bookingController.getBookingsByDateRange);
+router.get("/date-range", bookingController.getBookingsByDateRange);
 
 module.exports = router;
